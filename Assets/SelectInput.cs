f8c8f40a491570b0bc8f595d3e360eedc6495581
Mat <mat555333@gmail.com>
2020-06-23 11:11:37 +0200

    inputfield selection

---
+++ Assets/SelectInput.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SelectInput : MonoBehaviour
+{
+    // this is kind of bad. maybe i can override inputfield instead
+
+    [SerializeField] private TMP_InputField inputField;
+
+    private void Start()
+    {
+        SelectField();
+    }
+    public void SelectField()
+    {
+        inputField.ActivateInputField();
+    }
+}
